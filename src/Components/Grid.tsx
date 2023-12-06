6f00ea81a5b2dca56db5a0d892498dd9fe192a20
julien b 😺 <julien.berthou@homoludens.games>
2023-12-06 20:32:03 +0100

    Fix grid

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -61,7 +61,7 @@ export function Grid() {
   }
 
   return (
-    <animated.svg ref={ref} width="100%" style={{ touchAction: "none" }} height="100%" viewBox={viewBoxStr} xmlns="http://www.w3.org/2000/svg">
+    <animated.svg ref={ref} width="100%" height="100%" viewBox={viewBoxStr} xmlns="http://www.w3.org/2000/svg">
       <defs>
         <pattern id="grid" x="0" y="0" width="32" height="32" patternUnits="userSpaceOnUse">
           <line x1="16" y1="0" x2="16" y2="32" stroke="rgba(0,0,0,0.1)" strokeWidth="1" />
@@ -69,7 +69,7 @@ export function Grid() {
           <rect x="16" y="16" width="1" height="1" stroke="rgba(0,0,0,0.2)" strokeWidth="1" />
         </pattern>
       </defs>
-      <animated.rect x={xyz.to((x) => x)} y={xyz.to((x, y) => y)} {...bind()} width="100%" height="100%" fill="url(#grid)"></animated.rect>
+      <animated.rect x={xyz.to((x) => x)} y={xyz.to((x, y) => y)} {...bind()} width="100%" height="100%" fill="url(#grid)" style={{ touchAction: "none" }}></animated.rect>
       {edges.map((edge) => {
         return <Edge key={`${edge[0]}#${edge[1]} to ${edge[2]}#${edge[3]}`} start={getNodePort(edge[0] as string, edge[1] as string)} end={getNodePort(edge[2] as string, edge[3] as string)} type={edge[4] as PortType} />;
       })}
