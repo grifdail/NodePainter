be67cb6d3070e35c5b2def196c9690193fc089bf
julien b 😺 <julien.berthou@homoludens.games>
2024-03-20 10:43:50 +0100

    Fix pow node

--- src/Nodes/Math/PowNode.ts
+++ src/Nodes/Math/PowNode.ts
@@ -4,7 +4,7 @@ import { createOperation } from "../createOperation";
 
 export const PowNode: NodeDefinition = createOperation(
   "Pow",
-  (a, b) => a % b,
+  (a, b) => Math.pow(a, b),
   "Raise A to the power of B.",
   IconMathXPlusY,
   (a, b) => `pow(${a}, ${b})`
