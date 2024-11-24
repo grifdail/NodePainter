b0d796764b0237a981ff42aaea1e805627b62edf
Julien ✨ grifdail <julien@grifdail.fr>
2024-11-25 00:33:44 +0100

    Improve sorting !

--- src/Utils/sortAroundNode.ts
+++ src/Utils/sortAroundNode.ts
@@ -7,58 +7,38 @@ export const sortAroundNode = (tree: TreeStore, targetId: string) => {
   const rootNode = tree.nodes[targetId];
   const visited: { [id: string]: boolean } = {};
   const columns: { [id: number]: number } = {};
-  const recomputedPosition: { [id: string]: { x: number; y: number } } = { [rootNode.id]: { x: 0, y: 0 } };
+  const verticalMinPosition: { [id: string]: number | undefined } = { [rootNode.id]: 0 };
+  const nodePerColumn: { [id: string]: number } = { [rootNode.id]: 0 };
 
-  columns[0] = GetNodeHeight(rootNode, tree.getNodeTypeDefinition(rootNode)) + 100;
-
-  const sortInput = (parentNode: NodeData, column: number) => {
-    visited[parentNode.id] = true;
-    const nodeDef = tree.getNodeTypeDefinition(parentNode);
-    const nodeToTheLeft = getInputNodes(tree, parentNode, nodeDef);
-    nodeToTheLeft.forEach((node) => {
-      if (visited[node.id]) {
+  function parseInput(rootnode: NodeData, currentColumn: number) {
+    var inputs = getInputNodes(tree, rootnode, tree.getNodeTypeDefinition(rootnode));
+    let childHeight = 0;
+    inputs.forEach((node) => {
+      if (node.id === targetId) {
         return;
       }
-      const nodeDef = tree.getNodeTypeDefinition(node);
-      const height = GetNodeHeight(node, nodeDef) + 100;
-      const y = Math.max(+(columns[column] || 0), recomputedPosition[parentNode.id].y);
 
-      node.positionX = rootNode.positionX + column * 500;
-      node.positionY = rootNode.positionY + y;
-      columns[column] = y + height;
-      recomputedPosition[node.id] = { x: rootNode.positionX + column * 500, y: y };
-      if (visited[node.id]) {
-        return;
+      if (nodePerColumn[node.id] === undefined) {
+        nodePerColumn[node.id] = currentColumn - 1;
+      } else {
+        nodePerColumn[node.id] = Math.min(currentColumn - 1, nodePerColumn[node.id]);
       }
-      //sortOutput(node, column + 1);
-      sortInput(node, column - 1);
+      var height = parseInput(node, nodePerColumn[node.id]);
+      childHeight += height;
     });
-  };
+    return Math.max(GetNodeHeight(rootnode, tree.getNodeTypeDefinition(rootnode)), childHeight);
+  }
 
-  const sortOutput = (parentNode: NodeData, column: number) => {
-    visited[parentNode.id] = true;
-    const nodeDef = tree.getNodeTypeDefinition(parentNode);
-    const nodeToTheRight = getOutputNodes(tree, parentNode, nodeDef);
-    nodeToTheRight.forEach((node) => {
-      if (visited[node.id]) {
-        return;
-      }
-      const nodeDef = tree.getNodeTypeDefinition(node);
-      const height = GetNodeHeight(node, nodeDef) + 100;
-      const y = Math.max(+(columns[column] || 0), recomputedPosition[parentNode.id].y);
+  parseInput(rootNode, 0);
 
-      node.positionX = rootNode.positionX + column * 500;
-      node.positionY = rootNode.positionY + y;
-      columns[column] = y + height;
-      recomputedPosition[node.id] = { x: rootNode.positionX + column * 500, y: y };
-
-      sortInput(node, column - 1);
-      return sortOutput(node, column + 1);
-    });
-  };
-
-  sortInput(rootNode, -1);
-  sortOutput(rootNode, 1);
+  Object.entries(nodePerColumn).forEach(([key, columnId]) => {
+    const node = tree.nodes[key];
+    var height = GetNodeHeight(node, tree.getNodeTypeDefinition(node));
+    var column = columns[columnId] || 0;
+    node.positionX = rootNode.positionX + columnId * 500;
+    node.positionY = column;
+    columns[columnId] = column + height + 100;
+  });
 };
 function findNodeExecuting(tree: TreeStore, nodeId: string): NodeData[] {
   return Object.values(tree.nodes).filter((node) => Object.values(node.execOutputs).some((outputNode) => outputNode === nodeId));
@@ -66,17 +46,17 @@ function findNodeExecuting(tree: TreeStore, nodeId: string): NodeData[] {
 function findNodeUsing(tree: TreeStore, nodeId: string): NodeData[] {
   return Object.values(tree.nodes).filter((node) => Object.values(node.dataInputs).some((port) => port.hasConnection && port.connectedNode === nodeId));
 }
-function getInputNodes(tree: TreeStore, node: NodeData, nodeDef: NodeDefinition) {
+function getInputNodes(tree: TreeStore, node: NodeData, nodeDef: NodeDefinition): NodeData[] {
   var result = [];
-
+  if (nodeDef.canBeExecuted) {
+    result.push(...findNodeExecuting(tree, node.id));
+  }
   result.push(
     ...Object.values(node.dataInputs)
       .filter((port) => port.hasConnection)
       .map((port) => tree.nodes[port.connectedNode as string])
   );
-  if (nodeDef.canBeExecuted) {
-    result.push(...findNodeExecuting(tree, node.id));
-  }
+
   return result;
 }
 function getOutputNodes(tree: TreeStore, node: NodeData, nodeDef: NodeDefinition) {
