509e3ba9cdf53f37714bb002a53ccdb6608d7e02
Julien ✨ grifdail <julien@grifdail.fr>
2025-05-21 12:49:20 +0200

    Improve the UI for node input

--- src/Components/Modals/NodeSelectionModal.tsx
+++ src/Components/Modals/NodeSelectionModal.tsx
@@ -78,6 +78,19 @@ const CategoryButton = styled.button<{ selected?: boolean }>`
   border-radius: 24px;
   padding: var(--padding-small);
   text-transform: capitalize;
+  text-decoration: ${(props) => (props.selected ? "underline" : "none")};
+  font-weight: ${(props) => (props.selected ? "bold" : "none")};
+
+  &::after {
+    display: block;
+    content: attr(title);
+    font-weight: bold;
+    height: 1px;
+    color: transparent;
+    overflow: hidden;
+    visibility: hidden;
+    text-transform: capitalize;
+  }
 
   &:hover {
     background: var(--color-selected);
@@ -279,7 +292,8 @@ export function NodeSelectionModal({ close }: { close: () => void }) {
             <CategoryButton
               key={tag}
               selected={searchTerm.tags.includes(tag.toLowerCase())}
-              onClick={() => toggleTag(tag)}>
+              onClick={() => toggleTag(tag)}
+              title={tag}>
               {tag}
             </CategoryButton>
           ))}
