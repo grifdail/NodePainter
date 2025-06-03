1e1893af6d6b44b30851b96c78829147f2b3097f
Julien ✨ grifdail <julien@grifdail.fr>
2025-06-03 15:07:08 +0200

    Add uniform speed path setting

--- src/Components/Settings/PathSetting.tsx
+++ src/Components/Settings/PathSetting.tsx
@@ -5,11 +5,14 @@ import { PathSettingDefinition } from "../../Types/SettingDefinition";
 import { PathData } from "../../Types/PathData";
 import { MouseEvent, useState } from "react";
 import { current } from "immer";
+import { Fieldset } from "../StyledComponents/Fieldset";
+import { BoolInput } from "../Generics/Inputs/BoolInput";
 
 export const PathSetting: SettingComponent<PathSettingDefinition> = function PathSetting({ onChange, value, def }: SettingProps<PathSettingDefinition>) {
   const list = value as PathData;
   const [drawingStart, setDrawingStart] = useState<number | null>(null);
   const [onProgressPath, setOnProgressPath] = useState<PathData>([]);
+  const [uniformSpeed, setUniformSpeed] = useState(false);
 
   function onMouseDown(e: MouseEvent<SVGElement>) {
     setDrawingStart(Date.now());
@@ -17,7 +20,6 @@ export const PathSetting: SettingComponent<PathSettingDefinition> = function Pat
   }
   function onMouseUp(e: MouseEvent<SVGElement>) {
     if (drawingStart !== null) {
-      var t = Date.now() - drawingStart;
       onChange(onProgressPath);
       setDrawingStart(null);
     }
@@ -25,11 +27,21 @@ export const PathSetting: SettingComponent<PathSettingDefinition> = function Pat
   }
   function onMouseMouve(e: MouseEvent<SVGElement>) {
     if (drawingStart !== null) {
-      var t = Date.now() - drawingStart;
-      var rect = e.currentTarget.getBoundingClientRect();
-      var x = (e.clientX - rect.x) / rect.width;
-      var y = (e.clientY - rect.y) / rect.height;
-      setOnProgressPath((oldPath) => [...oldPath, x, y, t]);
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = (e.clientX - rect.x) / rect.width;
+      const y = (e.clientY - rect.y) / rect.height;
+      setOnProgressPath((oldPath) => {
+        let t = Date.now() - drawingStart;
+
+        if (uniformSpeed) {
+          const previousPoint = oldPath.length >= 3 ? [oldPath[oldPath.length - 3], oldPath[oldPath.length - 2], oldPath[oldPath.length - 1]] : [x, y, 0];
+          const dt = Math.sqrt((x - previousPoint[0]) * (x - previousPoint[0]) + (y - previousPoint[1]) * (y - previousPoint[1]));
+          t = previousPoint[2] + dt;
+          console.log(x, y, previousPoint, (x - previousPoint[0]) * (x - previousPoint[0]) + (y - previousPoint[1]) + (y - previousPoint[1]));
+        }
+
+        return [...oldPath, x, y, t];
+      });
     }
   }
   function onMouseLeave(e: MouseEvent<SVGElement>) {
@@ -53,11 +65,16 @@ export const PathSetting: SettingComponent<PathSettingDefinition> = function Pat
           strokeWidth="0.01"
           d={drawingStart ? generatePath(onProgressPath) : generatePath(list)}></path>
       </StyledPreview>
+      <Fieldset
+        label={"Uniform speed"}
+        input={BoolInput}
+        onChange={setUniformSpeed}
+        value={uniformSpeed}></Fieldset>
     </div>
   );
 };
 PathSetting.getSize = function (value, def): number {
-  return 272;
+  return 272 + 50;
 };
 
 var StyledPreview = styled.svg`
