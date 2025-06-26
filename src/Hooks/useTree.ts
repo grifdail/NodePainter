cf5cb233ec7d86a2703b42f83e8fb4fdb085a82e
Julien ✨ grifdail <julien@grifdail.fr>
2025-06-27 01:02:41 +0200

    Add Varying

--- src/Hooks/useTree.ts
+++ src/Hooks/useTree.ts
@@ -10,6 +10,7 @@ import { START_NODE } from "../Nodes/Misc/StartNode";
 import { NodeLibrary } from "../Nodes/Nodes";
 import { CUSTOM_SHADER } from "../Nodes/Shaders/RenderShader";
 import { SHADER_MATERIAL } from "../Nodes/Shaders/ShaderMaterial";
+import { VARYING } from "../Nodes/Shaders/ShaderMaterialStart";
 import { BoundingBox } from "../Types/BoundingBox";
 import { EDirection } from "../Types/EDirection";
 import { NodeCollection } from "../Types/NodeCollection";
@@ -357,7 +358,16 @@ export const useTree = create<TreeStore>()((set, get) => {
             id: start,
             tags: [],
             dataInputs: [],
-            dataOutputs: [...structuredClone(def.dataInputs)],
+            dataOutputs: [
+              ...Object.entries(VARYING).map(([id, data]): PortDefinition => {
+                return {
+                  id,
+                  type: data.type,
+                  defaultValue: PortTypeDefinitions[data.type].createDefaultValue(),
+                };
+              }),
+              ...structuredClone(def.dataInputs),
+            ],
 
             settings: [],
             executeAs: "ShaderMaterial-start",
--- src/Nodes/Shaders/ShaderMaterialStart.ts
+++ src/Nodes/Shaders/ShaderMaterialStart.ts
@@ -1,7 +1,27 @@
 import { IconPhoto } from "@tabler/icons-react";
 import { NodeDefinition } from "../../Types/NodeDefinition";
+import { PortType } from "../../Types/PortType";
 import { generateShaderCodeFromNodeData } from "../../Utils/graph/execution/generateShaderCodeFromNodeData";
 import { sanitizeForShader } from "../../Utils/graph/execution/sanitizeForShader";
+
+export const VARYING: Record<string, { type: PortType; identifier: string }> = {
+  worldPosition: {
+    type: "vector4",
+    identifier: "vWorldPosition",
+  },
+  uv: {
+    type: "vector2",
+    identifier: "vUv",
+  },
+  normal: {
+    type: "vector3",
+    identifier: "vNormal",
+  },
+  viewPosition: {
+    type: "vector3",
+    identifier: "vViewPosition",
+  },
+};
 
 export const ShaderMaterialStart: NodeDefinition = {
   id: "ShaderMaterial-start",
@@ -16,6 +36,9 @@ export const ShaderMaterialStart: NodeDefinition = {
   getShaderCode(node, context) {
     return [
       ...Object.values(node.dataOutputs).map((port) => {
+        if (VARYING[port.id]) {
+          return generateShaderCodeFromNodeData(node, context, port.id, {}, () => VARYING[port.id].identifier);
+        }
         return generateShaderCodeFromNodeData(node, context, port.id, {}, () => sanitizeForShader(`uniform_${port.id}`) as string);
       }),
     ].join("\n");
