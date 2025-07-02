245736de590ad51de909641b3013fd2f192fe14a
Julien ✨ grifdail <julien@grifdail.fr>
2025-07-03 01:15:07 +0200

    Fix OKLCH

--- src/Nodes/Color/OKLCH.ts
+++ src/Nodes/Color/OKLCH.ts
@@ -2,7 +2,6 @@ import { IconColorFilter } from "@tabler/icons-react";
 import convert from "color-convert";
 import { NodeDefinition } from "../../Types/NodeDefinition";
 import { createColor } from "../../Types/vectorDataType";
-import { clamp01 } from "../../Utils/math/clamp01";
 import { Constraints } from "../../Utils/ui/applyConstraints";
 
 export const OKLCH: NodeDefinition = {
@@ -11,9 +10,9 @@ export const OKLCH: NodeDefinition = {
   icon: IconColorFilter,
   tags: ["Color"],
   dataInputs: [
-    { id: "lightness", type: "number", defaultValue: 1, constrains: [Constraints.Clamp01()] },
-    { id: "chroma", type: "number", defaultValue: 1, constrains: [Constraints.Clamp01()] },
-    { id: "hue", type: "number", defaultValue: 0, constrains: [Constraints.Mod1()] },
+    { id: "lightness", type: "number", defaultValue: 50, constrains: [Constraints.Clamp(0, 100)] },
+    { id: "chroma", type: "number", defaultValue: 32, constrains: [Constraints.Clamp(0, 32)] },
+    { id: "hue", type: "number", defaultValue: 0, constrains: [Constraints.Clamp(0, 360)] },
     { id: "alpha", type: "number", defaultValue: 1, constrains: [Constraints.Clamp01()] },
   ],
   dataOutputs: [{ id: "color", type: "color", defaultValue: 1 }],
@@ -25,7 +24,7 @@ export const OKLCH: NodeDefinition = {
     var chroma = context.getInputValueNumber(nodeData, "chroma");
     var lightness = context.getInputValueNumber(nodeData, "lightness");
     const alpha = context.getInputValueNumber(nodeData, "alpha");
-    var result = (convert as any).oklch.rgb(clamp01(lightness) * 100, clamp01(chroma) * 32, clamp01(hue) * 360);
+    var result = (convert as any).oklch.rgb(lightness, chroma, hue);
     return createColor(result[0] / 255, result[1] / 255, result[2] / 255, alpha);
   },
 };
