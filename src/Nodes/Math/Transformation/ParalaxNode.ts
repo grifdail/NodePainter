5c927d79bac1157259c8272a62e2a9ea21143b16
julien <julien@grifdail.fr>
2026-01-25 19:25:19 +0100

    Add Paralax node

---
+++ src/Nodes/Math/Transformation/ParalaxNode.ts
@@ -0,0 +1,28 @@
+﻿import { IconArrowUpRightCircle } from "@tabler/icons-react";
+import { NodeDefinition } from "../../../Types/NodeDefinition";
+import { Port } from "../../../Types/PortTypeGenerator";
+import { createVector2 } from "../../../Types/vectorDataType";
+import { vectorAddition, vectorScale } from "../../../Utils/math/vectorUtils";
+
+export const ParallaxNode: NodeDefinition = {
+    id: "Math/Transformation/Parallax",
+    description: "Return the position of the element as if they were on multiple layer",
+    icon: IconArrowUpRightCircle,
+    tags: ["Vector", "Math"],
+    dataInputs: [
+        Port.vector2("position", createVector2()),
+        Port.number("layer", 0, "Item on layer 0 don't move. Positive number are in the back, negative number are on the front."),
+        Port.vector2("camera", createVector2(), "The position of the observer relative to layer 0"),
+    ],
+    dataOutputs: [
+        { id: "out", type: "vector2", defaultValue: createVector2() },
+    ],
+
+    settings: [],
+    getData: (portId, node, context) => {
+        const position = context.getInputValueVector2(node, "position");
+        const layer = context.getInputValueNumber(node, "layer");
+        const camera = context.getInputValueVector2(node, "camera");
+        return vectorAddition(position, vectorScale(camera, layer))
+    },
+};
--- src/Nodes/Math/Transformation/index.ts
+++ src/Nodes/Math/Transformation/index.ts
@@ -1,10 +1,12 @@
 import { FocalLengthNode } from "./FocalLengthNode";
 import { HexGridNode } from "./HexGridNode";
+import { ParallaxNode } from "./ParalaxNode";
 import { TwirlNode } from "./TwirlNode";
 
 export const MathTransformNodes = [
-  //
-  FocalLengthNode,
-  HexGridNode,
-  TwirlNode,
+    //
+    FocalLengthNode,
+    HexGridNode,
+    ParallaxNode,
+    TwirlNode,
 ];
